adc911e2bef1b97c078279e01df25110ccae8973
Kuroakira <jinnseitanosi@gmail.com>
2025-12-04 20:09:05 +0900

    refactor: simplify GildedRose item handling by replacing conditional checks with a switch statement for specific items

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -32,9 +32,6 @@ class BackstagePasses extends Item {
 
     this.quality = this.quality - this.quality;
   }
-  static isBackstagePasses(name: string): name is "Backstage passes to a TAFKAL80ETC concert" {
-    return name === SpecificItemNames.BACKSTAGE_PASSES;
-  }
 }
 
 class AgedBrie extends Item {
@@ -55,9 +52,6 @@ class AgedBrie extends Item {
       this.quality = this.quality + 1;
     }
   }
-  static isAgedBrie(name: string): name is "Aged Brie" {
-    return name === SpecificItemNames.AGED_BRIE;
-  }
 }
 
 class Sulfuras extends Item {
@@ -70,19 +64,13 @@ class Sulfuras extends Item {
     }
     return;
   }
-  static isSulfuras(name: string): name is "Sulfuras, Hand of Ragnaros" {
-    return name === SpecificItemNames.SULFURAS;
-  }
 }
-
 
 const SpecificItemNames = {
   AGED_BRIE: 'Aged Brie',
   BACKSTAGE_PASSES: 'Backstage passes to a TAFKAL80ETC concert',
   SULFURAS: 'Sulfuras, Hand of Ragnaros',
 }
-
-const SPECIFIC_ITEMS = Object.values(SpecificItemNames)
 
 export class GildedRose {
   items: Array<Item>;
@@ -95,50 +83,37 @@ export class GildedRose {
     for (let i = 0; i < this.items.length; i++) {
       const itemName = this.items[i].name;
 
-      if (BackstagePasses.isBackstagePasses(itemName)) {
-        const backstagePasses = new BackstagePasses(this.items[i].sellIn, this.items[i].quality);
-        backstagePasses.update();
-        this.items[i].quality = backstagePasses.quality;
-        this.items[i].sellIn = backstagePasses.sellIn;
-        continue;
-      }
-
-      if (AgedBrie.isAgedBrie(itemName)) {
-        const agedBrie = new AgedBrie(this.items[i].sellIn, this.items[i].quality);
-        agedBrie.update();
-        this.items[i].quality = agedBrie.quality;
-        this.items[i].sellIn = agedBrie.sellIn;
-        continue;
-      }
-
-      if (Sulfuras.isSulfuras(itemName)) {
-        const sulfuras = new Sulfuras(this.items[i].sellIn, this.items[i].quality);
-        sulfuras.update();
-        this.items[i].quality = sulfuras.quality;
-        this.items[i].sellIn = sulfuras.sellIn;
-        continue;
-      }
-
-      if (
-        !SPECIFIC_ITEMS.includes(itemName) &&
-        this.items[i].quality > 0
-      ) {
-        this.items[i].quality = this.items[i].quality - 1
-      }
-
-      if (itemName !== SpecificItemNames.SULFURAS) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-
-      if (this.items[i].sellIn >= 0) {
-        continue;
-      }
-
-      if (
-        this.items[i].quality > 0 &&
-        itemName !== SpecificItemNames.SULFURAS
-      ) {
-        this.items[i].quality = this.items[i].quality - 1
+      switch (itemName) {
+        case SpecificItemNames.BACKSTAGE_PASSES:
+          const backstagePasses = new BackstagePasses(this.items[i].sellIn, this.items[i].quality);
+          backstagePasses.update();
+          this.items[i].quality = backstagePasses.quality;
+          this.items[i].sellIn = backstagePasses.sellIn;
+          break;
+        case SpecificItemNames.AGED_BRIE:
+          const agedBrie = new AgedBrie(this.items[i].sellIn, this.items[i].quality);
+          agedBrie.update();
+          this.items[i].quality = agedBrie.quality;
+          this.items[i].sellIn = agedBrie.sellIn;
+          break;
+        case SpecificItemNames.SULFURAS:
+          const sulfuras = new Sulfuras(this.items[i].sellIn, this.items[i].quality);
+          sulfuras.update();
+          this.items[i].quality = sulfuras.quality;
+          this.items[i].sellIn = sulfuras.sellIn;
+          break;
+        default:
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1;
+          }
+          this.items[i].sellIn = this.items[i].sellIn - 1;
+          if (this.items[i].sellIn >= 0) {
+            continue;
+          }
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1;
+          }
+          break;
       }
     }
 
