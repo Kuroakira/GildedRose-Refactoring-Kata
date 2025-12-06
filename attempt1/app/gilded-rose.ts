f7391cc375222217805ded9d30f67c885fc775e4
Kuroakira <jinnseitanosi@gmail.com>
2025-12-06 10:13:01 +0900

    refactor: improve AgedBrie update logic to correctly increase quality based on sellIn value

--- attempt1/app/gilded-rose.ts
+++ attempt1/app/gilded-rose.ts
@@ -39,18 +39,18 @@ class BackstagePasses extends Item {
 
 class AgedBrie extends Item {
   update() {
-    if (this.quality < 50) {
-      this.quality = this.quality + 1;
-    }
     this.sellIn = this.sellIn - 1;
 
-    if (this.sellIn >= 0) {
+    if (this.quality >= 50) {
       return;
     }
 
-    if (this.quality < 50) {
+    if (this.sellIn >= 0) {
       this.quality = this.quality + 1;
+      return;
     }
+
+    this.quality = this.quality + 2;
   }
 }
 
