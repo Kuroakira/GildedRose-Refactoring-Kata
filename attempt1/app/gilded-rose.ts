d6a490c20112735e5297c351bf4722eb97195453
Kuroakira <jinnseitanosi@gmail.com>
2025-12-06 14:20:29 +0900

    refactor: streamline GildedRose updateQuality method by removing redundant Sulfuras checks

--- attempt1/app/gilded-rose.ts
+++ attempt1/app/gilded-rose.ts
@@ -25,8 +25,12 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name === SPECIFIC_ITEMS.SULFURAS) {
+        continue;
+      }
+
       if (this.items[i].name != SPECIFIC_ITEMS.AGED_BRIE && this.items[i].name != SPECIFIC_ITEMS.BACKSTAGE_PASSES) {
-        if (this.items[i].quality > 0 && this.items[i].name != SPECIFIC_ITEMS.SULFURAS) {
+        if (this.items[i].quality > 0) {
           this.items[i].quality = this.items[i].quality - 1
         }
       } else {
@@ -42,13 +46,13 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != SPECIFIC_ITEMS.SULFURAS) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+
+      this.items[i].sellIn = this.items[i].sellIn - 1;
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != SPECIFIC_ITEMS.AGED_BRIE) {
           if (this.items[i].name != SPECIFIC_ITEMS.BACKSTAGE_PASSES) {
-            if (this.items[i].quality > 0 && this.items[i].name != SPECIFIC_ITEMS.SULFURAS) {
+            if (this.items[i].quality > 0) {
               this.items[i].quality = this.items[i].quality - 1
             }
           } else {
