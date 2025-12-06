b8a18921dc5cadce15d35b6df2f818dc62e00b62
Kuroakira <jinnseitanosi@gmail.com>
2025-12-06 09:22:37 +0900

    refactor: implement update logic directly in Item class and streamline item creation in GildedRose

--- attempt1/app/gilded-rose.ts
+++ attempt1/app/gilded-rose.ts
@@ -9,8 +9,17 @@ export class Item {
     this.quality = quality;
   }
 
-  update(): void {
-    throw new Error("Not implemented");
+  update() {
+    if (this.quality > 0) {
+      this.quality = this.quality - 1;
+    }
+    this.sellIn = this.sellIn - 1;
+    if (this.sellIn >= 0) {
+      return;
+    }
+    if (this.quality > 0) {
+      this.quality = this.quality - 1;
+    }
   }
 }
 
@@ -32,21 +41,6 @@ class BackstagePasses extends Item {
     }
 
     this.quality = this.quality - this.quality;
-  }
-}
-
-class NormalItem extends Item {
-  update() {
-    if (this.quality > 0) {
-      this.quality = this.quality - 1;
-    }
-    this.sellIn = this.sellIn - 1;
-    if (this.sellIn >= 0) {
-      return;
-    }
-    if (this.quality > 0) {
-      this.quality = this.quality - 1;
-    }
   }
 }
 
@@ -76,42 +70,34 @@ class Sulfuras extends Item {
   }
 }
 
-class ItemFactory {
-  static createItem(name: string, sellIn: number, quality: number): Item {
-    switch (name) {
-      case SpecificItemNames.AGED_BRIE:
-        return new AgedBrie(name, sellIn, quality);
-      case SpecificItemNames.BACKSTAGE_PASSES:
-        return new BackstagePasses(name, sellIn, quality);
-      case SpecificItemNames.SULFURAS:
-        return new Sulfuras(name, sellIn, quality);
-      default:
-        return new NormalItem(name, sellIn, quality);
-    }
-  }
-}
-
 const SpecificItemNames = {
   AGED_BRIE: 'Aged Brie',
   BACKSTAGE_PASSES: 'Backstage passes to a TAFKAL80ETC concert',
   SULFURAS: 'Sulfuras, Hand of Ragnaros',
 }
 
+const NameToClass: { [key: string]: typeof Item} = {
+  [SpecificItemNames.AGED_BRIE]: AgedBrie,
+  [SpecificItemNames.BACKSTAGE_PASSES]: BackstagePasses,
+  [SpecificItemNames.SULFURAS]: Sulfuras,
+};
+
 export class GildedRose {
+  private CommonClass: typeof Item = Item;
   items: Array<Item>;
 
   constructor(items = [] as Array<Item>) {
-    this.items = items;
+    this.items = items.map(item => {
+      const Class: typeof Item = NameToClass[item.name] || this.CommonClass;
+      return new Class(item.name, item.sellIn, item.quality);
+    });
   }
 
   updateQuality() {
     // The golden master test is using the original items array, so we need to keep the original array's references.
     // If we create a new item, it will not be the same object as the original item, and the golden master test will fail.
     for (let i = 0; i < this.items.length; i++) {
-      const item = ItemFactory.createItem(this.items[i].name, this.items[i].sellIn, this.items[i].quality);
-      item.update();
-      this.items[i].quality = item.quality;
-      this.items[i].sellIn = item.sellIn;
+      this.items[i].update();
     }
 
     return this.items;
