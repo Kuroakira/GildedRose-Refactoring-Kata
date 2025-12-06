41051a8a2d68572a9de79651664ec0186a24d5a2
Kuroakira <jinnseitanosi@gmail.com>
2025-12-06 10:15:54 +0900

    reset all

--- attempt1/app/gilded-rose.ts
+++ attempt1/app/gilded-rose.ts
@@ -3,105 +3,65 @@ export class Item {
   sellIn: number;
   quality: number;
 
-  constructor(name: string, sellIn: number, quality: number) {
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
-
-  update(): void {
-    throw new Error("Not implemented");
-  }
 }
-
-class BackstagePasses extends Item {
-  update() {
-    if (this.sellIn < 0) {
-      this.quality = 0;
-      return;
-    }
-
-    if (this.sellIn >= 11) {
-      this.quality = this.quality + 1;
-    } else if (this.sellIn < 11 && this.sellIn >= 6) {
-      this.quality = this.quality + 2;
-    } else {
-      this.quality = this.quality + 3;
-    }
-
-    if (this.quality >= 50) {
-      this.quality = 50;
-    }
-
-    this.sellIn = this.sellIn - 1;
-  }
-}
-
-class AgedBrie extends Item {
-  update() {
-    this.sellIn = this.sellIn - 1;
-
-    if (this.quality >= 50) {
-      return;
-    }
-
-    if (this.sellIn >= 0) {
-      this.quality = this.quality + 1;
-      return;
-    }
-
-    this.quality = this.quality + 2;
-  }
-}
-
-class Sulfuras extends Item {
-  update() {
-    // Sulfuras does not change
-    return;
-  }
-}
-
-class NormalItem extends Item {
-  update() {
-    if (this.quality > 0) {
-      this.quality = this.quality - 1;
-    }
-    this.sellIn = this.sellIn - 1;
-    if (this.sellIn >= 0) {
-      return;
-    }
-    if (this.quality > 0) {
-      this.quality = this.quality - 1;
-    }
-  }
-}
-
-const SpecificItemNames = {
-  AGED_BRIE: 'Aged Brie',
-  BACKSTAGE_PASSES: 'Backstage passes to a TAFKAL80ETC concert',
-  SULFURAS: 'Sulfuras, Hand of Ragnaros',
-}
-
-const NameToClass: { [key: string]: typeof Item} = {
-  [SpecificItemNames.AGED_BRIE]: AgedBrie,
-  [SpecificItemNames.BACKSTAGE_PASSES]: BackstagePasses,
-  [SpecificItemNames.SULFURAS]: Sulfuras,
-};
 
 export class GildedRose {
-  private CommonClass: typeof Item = NormalItem;
   items: Array<Item>;
 
   constructor(items = [] as Array<Item>) {
-    this.items = items.map(item => {
-      const Class: typeof Item = NameToClass[item.name] || this.CommonClass;
-      return new Class(item.name, item.sellIn, item.quality);
-    });
+    this.items = items;
   }
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      this.items[i].update();
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].quality > 0) {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1
+          }
+        }
+      } else {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1
+          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].sellIn < 11) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+            if (this.items[i].sellIn < 6) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+          }
+        }
+      }
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+      }
+      if (this.items[i].sellIn < 0) {
+        if (this.items[i].name != 'Aged Brie') {
+          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].quality > 0) {
+              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality = this.items[i].quality - 1
+              }
+            }
+          } else {
+            this.items[i].quality = this.items[i].quality - this.items[i].quality
+          }
+        } else {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1
+          }
+        }
+      }
     }
 
     return this.items;
--- attempt1/test/jest/gilded-rose.spec.ts
+++ attempt1/test/jest/gilded-rose.spec.ts
@@ -4,16 +4,6 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
-    expect(items[0].quality).toBe(0);
-    expect(items[0].sellIn).toBe(-1);
-  });
-
-  it('should Backstage with sellIn under 11', () => {
-    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('Backstage passes to a TAFKAL80ETC concert');
-    expect(items[0].quality).toBe(2);
-    expect(items[0].sellIn).toBe(9);
+    expect(items[0].name).toBe('fixme');
   });
 });
